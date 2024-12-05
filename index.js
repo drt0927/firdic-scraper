07ba8b2841314effb01c5480d3024160971039f5
filbert <drt_0927@naver.com>
2024-12-05 07:09:31 +0000

    테스트

---
+++ index.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const baseUrl = 'https://open.api.nexon.com/static/tfd/meta';
+const apiKey = 'test_645470676a3bbb55286e489eef40ae9ecc9cc84a179a54abf948923c76d9d94cefe8d04e6d233bd35cf2fabdeb93fb0d';
+const jsonList = [
+    '/ko/descendant.json',
+    '/ko/weapon.json',
+    '/ko/module.json',
+    '/ko/reactor.json',
+    '/ko/external-component.json',
+    '/ko/reward.json',
+    '/ko/stat.json',
+    '/ko/void-battle.json',
+    '/ko/title.json',
+    '/descendant-level-detail.json',
+    '/mastery-rank-level-detail.json',
+    '/ko/consumable-material.json',
+    '/ko/research.json',
+    '/amorphous-reward.json',
+    '/amorphous-reward-group.json',
+    '/ko/amorphous-open-condition-description.json',
+    '/ko/acquisition-detail.json',
+    '/ko/weapon-type.json'
+];
+
+function Request(jsonName) {
+    return axios.get(`${baseUrl}${jsonName}`, {
+        headers: {
+            'x-nxopen-api-key': apiKey
+        },
+        maxBodyLength: Infinity
+    });
+}
+
+function main() {
+    const folderName = 'dist/tfd';
+
+    // 폴더 없는 경우 폴더 생성
+    if (!fs.existsSync(folderName)) {
+        fs.mkdirSync(folderName, {
+            recursive: true
+        });
+    }
+
+    const jobList = [];
+    for (const j of jsonList) {
+        jobList.push(Request(j));
+    }
+
+    Promise.all(jobList)
+        .then((values) => {
+            for (const value of values) {
+                const names = value.request.path.split('/');
+                const fileName = names[names.length - 1];
+                const json = JSON.stringify(value.data);
+                fs.writeFileSync(`${folderName}/${fileName}`, json, 'utf-8');
+            }
+
+            console.log('😀Success!!!😀');
+        })
+        .catch(err => {
+            console.log('😢Fail!!!😢');
+            throw err;
+        });
+}
+
+main();
